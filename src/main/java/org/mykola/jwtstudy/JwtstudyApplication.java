abd296cdaf9be4f344dc7eb35765ddb1707983de
Grigorich <oskinnick@gmail.com>
2023-01-26 18:06:43 +0300

    Initial commit

---
+++ src/main/java/org/mykola/jwtstudy/JwtstudyApplication.java
@@ -0,0 +1,13 @@
+package org.mykola.jwtstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtstudyApplication.class, args);
+    }
+
+}
