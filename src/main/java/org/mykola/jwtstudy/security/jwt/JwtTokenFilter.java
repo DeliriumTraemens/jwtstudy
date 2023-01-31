a5161f22773c3d92d72e82f2ab0ee3cea3a6e004
Grigorich <oskinnick@gmail.com>
2023-01-31 14:34:30 +0300

    JwtTokenFilter Completed

--- src/main/java/org/mykola/jwtstudy/security/jwt/JwtTokenFilter.java
+++ src/main/java/org/mykola/jwtstudy/security/jwt/JwtTokenFilter.java
@@ -1,6 +1,9 @@
 package org.mykola.jwtstudy.security.jwt;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
+import sun.plugin.liveconnect.SecurityContextHelper;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -19,5 +22,13 @@ public class JwtTokenFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) servletRequest);
+        if(token != null && jwtTokenProvider.validateToken(token)){
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                if (authentication !=null){
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+
     }
 }
