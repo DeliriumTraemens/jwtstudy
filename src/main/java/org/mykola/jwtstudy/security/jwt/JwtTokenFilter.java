3dcb12bdbdfd6094fc9e7bce89d7d9153b82c7b3
Grigorich <oskinnick@gmail.com>
2023-01-31 14:25:55 +0300

    JwtTokenProvider completed, JwtTokenFilter started J

---
+++ src/main/java/org/mykola/jwtstudy/security/jwt/JwtTokenFilter.java
@@ -0,0 +1,23 @@
+package org.mykola.jwtstudy.security.jwt;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class JwtTokenFilter extends GenericFilterBean {
+    JwtTokenProvider jwtTokenProvider;
+
+    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) servletRequest);
+    }
+}
